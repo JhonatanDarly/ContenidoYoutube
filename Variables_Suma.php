7bce4624331af2923cfe78640814ecc212c1b732
JhonatanDarly <jhonatanmolinachinchay@gmail.com>
2025-05-31 22:15:55 -0500

    Add files via upload

---
+++ Variables_Suma.php
@@ -0,0 +1,23 @@
+<?php
+ $variableuno = "Hola mundo";
+ 
+ $numero_uno 		= 50.10;
+ $numero_dos 		= 4.20;
+ $numero_uno_po		= 8;
+ $numero_dos_po		= 2;
+ 
+ $suma 				= $numero_uno + $numero_dos;
+ $resta 			= $numero_uno - $numero_dos;
+ $multiplicacio 	= $numero_uno * $numero_dos;
+ $division			= $numero_uno / $numero_dos;
+ $potenciacion		= pow($numero_dos_po, $numero_uno_po);
+ echo "Este es un mensaje con variables:".$variableuno."<br>";
+ echo "Suma uno realizada =".$suma."<br>";
+ echo "<b>Suma</b> dos realizado =".$suma = $numero_uno + $numero_dos."</br>";
+ echo "<b>Resta</b> de variables numeros enteros= ".$resta."<br>";
+ echo "<b> Multiplicacion = </b>".$multiplicacio."<br>";
+ echo "<b> Division= </b>".$division."<br>";
+ echo "<b>Potenciacion =</b>".$potenciacion."<br>";
+
+
+?>
